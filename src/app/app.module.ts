805acf70316b4d573aeb82a79aba7fbaa69ff9be
Esteban Beerens <esteban.beerens@gmail.com>
2020-04-09 17:42:55 +0200

    Edited TS to include right files and make navbar change color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { DIYComponent } from './diy/diy.component';
 import { TMComponent } from './tm/tm.component';
