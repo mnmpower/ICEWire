2614e33d2331fe26dd6b94d88e3e1c47454e18ed
Maarten Michiels <mnmmaarten@hotmail.com>
2020-04-09 12:19:42 +0200

    Added Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'signUp', component: AppComponent },
-  { path: 'logIn', component: AppComponent },
+  { path: '', component: HomeComponent },
+  { path: 'signUp', component: HomeComponent },
+  { path: 'logIn', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
